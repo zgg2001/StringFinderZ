37b4d1a37a9daaa218ed6185bee2c2e34574b946
zgg2001 <397606244@qq.com>
2021-10-21 19:59:58 +0800

    v1.1 参数更新

--- src/argument/argument.go
+++ src/argument/argument.go
@@ -6,6 +6,8 @@ type Argument struct {
     Replace string  //替换内容
     IsFind  bool    //是否为找到模式
     Number  bool    //显示行号
+    AllYes  bool    //是否全部许可替换
+    Quick   bool    //是否快速替换
 }
 
 var Arg = &Argument{}
